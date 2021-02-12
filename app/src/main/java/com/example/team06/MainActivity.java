a60666e36164611a850c8967c37a0079aa0270fb
grog25 <73005196+grog25@users.noreply.github.com>
2021-02-11 22:08:08 -0700

    removed classes and put them in the main, tried to get the textView and textView2 to work but couldn't. If textView & sendMessage is commented out, it works.

--- app/src/main/java/com/example/team06/MainActivity.java
+++ app/src/main/java/com/example/team06/MainActivity.java
@@ -1,35 +1,95 @@
 package com.example.team06;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Odds odds;
-        odds = new Odds();
-        Evens evens;
-        evens = new Evens();
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        try {
-            new Thread(odds);
-            Thread.sleep(250);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            new Thread(evens);
-            Thread.sleep(250);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
     }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        Log.i(TAG, "About to create intent with "+ textView.getText().toString() +
+                textView2.getText().toString());
+
+        String odds = textView.getText().toString();
+        String evens = textView2.getText().toString();
+
+        intent.putExtra(ODDS, odds);
+        intent.putExtra(EVENS, evens);
+
+        startActivity(intent);
+    }
+
+        public void odds(View view) {
+            ExampleThread thread = new ExampleThread(100);
+            thread.start();
+        }
+
+        public void evens(View view) {
+            ExampleRunnable runnable = new ExampleRunnable(100);
+            new Thread(runnable).start();
+
+        }
+
+        class ExampleThread extends Thread {
+            int count;
+
+            ExampleThread(int count) {
+                this.count = count;
+            }
+
+            @Override
+            public void run() {
+                for (int i = 1; i <= count; i++) {
+                    if (i % 2 != 0)
+                    Log.d(TAG, "startThread: " + i);
+                    try {
+                        Thread.sleep(250);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    TextView textView = findViewById(R.id.textView);
+                    textView.setText(i);
+                }
+            }
+        }
+        class ExampleRunnable implements Runnable {
+            int count;
+
+
+
+            ExampleRunnable(int count) {
+                this.count = count;
+            }
+            @Override
+            public void run() {
+                for (int i = 0; i <= count; i++) {
+                    if (i % 2 == 0)
+                        Log.d(TAG, "startThread: " + i);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    TextView textView2 = findViewById(R.id.textView2);
+                    textView2.setText(i);
+                }
+            }
+        }
 }
